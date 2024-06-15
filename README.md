2a5d02ce247d08aa0a1325c37202332a018c5a31
Valeriy Golyshev <valeriy1golyshev@gmail.com>
2024-06-15 18:22:49 +0700

    Created project

---
+++ README.md
@@ -0,0 +1,3 @@
+# school_todo_list
+
+Flutter проект мобильного приложения "TODO лист".
---
+++ android/app/src/main/kotlin/com/example/school_todo_list/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.school_todo_list
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
